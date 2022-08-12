d97deeb6c09f309c225aa9aad08ed0437ee9994a
Leonan Costa <leonanfecosta@gmail.com>
2022-08-12 12:42:33 -0300

    feat: cria as variaveis de ambiente

--- controllers/connection.js
+++ controllers/connection.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATA_BASE, DB_PORT } = process.env;
 
 const connection = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
+  host: DB_HOST || 'localhost',
+  port: DB_PORT || 3306,
+  user: DB_USER || 'root',
+  password: DB_PASSWORD || 'password',
+  database: DB_DATA_BASE || 'StoreManager',
 });
 
 module.exports = connection;
